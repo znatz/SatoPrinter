70b436b98cd829e25e911ca13c703c6f9669e3f0
znatz <z.nat.cn@gmail.com>
2015-09-25 13:30:31 +0900

    Initial Commit

---
+++ SatoPrinter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SatoPrinter
+//
+//  Created by 小泉 丈太 on 2015/09/25.
+//  Copyright © 2015年 小泉 丈太. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
